104d2815cc7fd4da796e98770fe0eb835ae7af23
Marcus <marcus.kariuki@student.moringaschool.com>
2023-03-01 12:57:57 +0300

    initial file set up create landing page add navbar and sections to landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
 import '../App.css';
+import Header from './Header';
+import { Container, Grid, Header as SemanticHeader, List, Segment } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div className='my-app'>
+    <Header/>
+
+    <Container text style={{ marginTop: '17em' }}>
+      <h2>About us</h2>
+    </Container>
+
+    <Container text style={{ marginTop: '17em' }}>
+      <h2>Services</h2>
+    </Container>
+
+    <Container text style={{ marginTop: '17em' }}>
+      <h2>Faqs</h2>
+    </Container>
+
+    <Container text style={{ marginTop: '17em' }}>
+      <h2>Footer</h2>
+    </Container>
+
+
+
+
+    </div>
   );
 }
 
